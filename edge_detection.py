fa55fb92f2aadd58f27a84f3bd2ef2933207cda2
Serena Cheng <serena.ch1999@gmail.com>
2022-04-09 22:10:03 -0400

    Fix operator matrix

--- edge_detection.py
+++ edge_detection.py
@@ -3,14 +3,14 @@ import numpy as np
 import sub_matrices
 
 
-def apply_operator(img, op_x, op_y):
+def apply_operator(img, operator):
     filtered_matrix = []
 
-    mask_size_matrices = sub_matrices.get_mask_size_matrices(img, op_x.shape[0])
+    mask_size_matrices = sub_matrices.get_mask_size_matrices(img, operator.shape[0])
 
     for matrix in mask_size_matrices:
-        px_x = np.sum(np.multiply(matrix, op_x))
-        px_y = np.sum(np.multiply(matrix, op_y))
+        px_x = np.sum(np.multiply(matrix, operator))
+        px_y = np.sum(np.multiply(matrix, operator.T))
         filtered_matrix.append(np.sqrt(px_x ** 2 + px_y ** 2))
 
     filt_img = np.array(filtered_matrix).reshape(img.shape).astype(np.uint8)
@@ -19,26 +19,20 @@ def apply_operator(img, op_x, op_y):
 
 
 def prewitt_operator(img):
-    op_x = np.array([[-1, 0, 1],
+    prewitt = np.array([[-1, 0, 1],
                      [-1, 0, 1],
                      [-1, 0, 1]])
-    op_y = np.array([[-1, -1, -1],
-                     [0, 0, 0],
-                     [1, 1, 1]])
 
-    edge_img = apply_operator(img, op_x, op_y)
+    edge_img = apply_operator(img, prewitt)
 
     return edge_img
 
 
 def sobel_operator(img):
-    op_x = np.array([[-1, 0, 1],
+    sobel = np.array([[-1, 0, 1],
                      [-2, 0, 2],
                      [-1, 0, 1]])
-    op_y = np.array([[-1, -2, -1],
-                     [0, 0, 0],
-                     [1, 2, 1]])
 
-    edge_img = apply_operator(img, op_x, op_y)
+    edge_img = apply_operator(img, sobel)
 
     return edge_img
